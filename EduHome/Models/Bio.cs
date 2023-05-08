8841bc87aa9ba469cd02d3a376ec9ca83c1fb07e
MuradHacizada <muradhacizade8155@gmail.com>
2023-05-09 02:07:41 +0400

    Edu Home created

---
+++ EduHome/Models/Bio.cs
@@ -0,0 +1,16 @@
+﻿namespace EduHome.Models
+{
+    public class Bio
+    {
+        public int Id { get; set; }
+        public string HeaderNumber { get; set; }
+        public string HeaderLogo{ get; set; }
+        public string FooterLogo { get; set; }
+        public string Address { get; set; }
+        public string FootNum1 { get; set; }
+        public string FootNum2 { get; set; }
+        public string Email { get; set; }
+        public string Description { get; set; }
+
+    }
+}
