e0ff7f7a252b18741fae07139c55ed9111bb8b8f
MuradHacizada <muradhacizade8155@gmail.com>
2023-05-09 04:24:40 +0400

    Admin panel added and Service update button active

---
+++ EduHome/Areas/Admin/Controllers/ServicesController.cs
@@ -0,0 +1,80 @@
+﻿using EduHome.DAL;
+using EduHome.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EduHome.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ServicesController : Controller
+    {
+        private readonly AppDbContext _db;
+        public ServicesController(AppDbContext db)
+        {
+                _db = db;   
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Service> services = await _db.Services.ToListAsync();
+            return View(services);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Service service)
+        {
+            bool isExist=await _db.Services.AnyAsync(x=>x.Name==service.Name);
+            if (isExist) 
+            {
+                ModelState.AddModelError("Name", "This Service HAVE PLS ANTOHER WRITE !");
+                return View();
+            }
+            await _db.Services.AddAsync(service);
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+        public async Task<IActionResult> Update(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            
+            Service dbService =await _db.Services.FirstOrDefaultAsync(x => x.Id == id);
+            if (dbService == null)
+            {
+                return BadRequest();
+            }
+            return View(dbService);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Update(int? id,Service service)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Service dbService = await _db.Services.FirstOrDefaultAsync(x => x.Id == id);
+            if (dbService == null)
+            {
+                return BadRequest();
+            }
+            bool isExist = await _db.Services.AnyAsync(x => x.Name == service.Name && x.Id!=id);
+            if (isExist)
+            {
+                ModelState.AddModelError("Name", "This Service HAVE PLS ANTOHER WRITE !");
+                return View();
+            }
+            dbService.Name=service.Name;
+            dbService.Description=service.Description;
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
